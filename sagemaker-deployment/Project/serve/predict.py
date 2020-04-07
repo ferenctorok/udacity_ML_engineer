9a849a84433f9bdc74a26ecf143fbaa9cc6e9a4a
EC2 Default User <ec2-user@ip-172-16-70-90.eu-west-1.compute.internal>
2020-04-07 14:56:42 +0000

    Project is mostly done, some issues with the web app.

--- sagemaker-deployment/Project/serve/predict.py
+++ sagemaker-deployment/Project/serve/predict.py
@@ -55,7 +55,7 @@ def input_fn(serialized_input_data, content_type):
 
 def output_fn(prediction_output, accept):
     print('Serializing the generated output.')
-    return str(prediction_output)
+    return str(prediction_output).encode('utf-8')
 
 def predict_fn(input_data, model):
     print('Inferring sentiment of input data.')
@@ -69,9 +69,8 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data), pad=500)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +86,6 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    result = float(model.forward(data))
 
     return result
