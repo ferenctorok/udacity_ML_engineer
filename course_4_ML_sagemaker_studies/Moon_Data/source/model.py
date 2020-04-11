0cd3d2959eb33f0c2bf4d09bcdf66d4d30ebc6ec
EC2 Default User <ec2-user@ip-172-16-83-41.eu-west-1.compute.internal>
2020-04-11 18:36:42 +0000

    moon data is ready untily training

--- course_4_ML_sagemaker_studies/Moon_Data/source/model.py
+++ course_4_ML_sagemaker_studies/Moon_Data/source/model.py
@@ -14,8 +14,18 @@ class SimpleNet(nn.Module):
          '''
         super(SimpleNet, self).__init__()
         
-        # define all layers, here
+        # your code here:
+        # defining layers:
+        self.fc_1 = nn.Linear(in_features=input_dim, out_features=hidden_dim, bias=True)
+        self.act_1 = nn.ReLU()
+        self.fc_2 = nn.Linear(in_features=hidden_dim, out_features=output_dim, bias=True)
+        self.act_2 = nn.Sigmoid()
         
+        # initializing layers:
+        self.fc_1.weight = nn.init.xavier_uniform_(self.fc_1.weight, gain=nn.init.calculate_gain('relu'))
+        self.fc_1.bias = nn.init.zeros_(self.fc_1.bias)
+        self.fc_2.weight = nn.init.xavier_uniform_(self.fc_2.weight, gain=1.0)
+        self.fc_2.bias = nn.init.zeros_(self.fc_2.bias)
     
     ## TODO: Define the feedforward behavior of the network
     def forward(self, x):
@@ -23,6 +33,8 @@ class SimpleNet(nn.Module):
            :param x: A batch of input features
            :return: A single, sigmoid activated value
          '''
-        # your code, here
+        # your code here:
+        x = self.act_1(self.fc_1(x))
+        x = self.act_2(self.fc_2(x))
         
         return x
